3c7bb2ae4d2f7d5a032447cf25efe4598fb40830
diogohf <diogohenriquefrsz@gmail.com>
2025-06-11 20:29:23 -0700

    update

---
+++ codes/mean_roman_nz.py
@@ -0,0 +1,63 @@
+"""
+Diogo H. F. de Souza - Fri Mar 28 2025
+Get the mean of redshift distribution for 9 millions of Roman scenarios
+"""
+
+## LIBS ##
+# import fitsio as fio
+import numpy as np
+from numpy import linalg as la
+from numpy.lib.recfunctions import stack_arrays
+import matplotlib
+matplotlib.use ('agg')
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.colors import LogNorm
+import matplotlib.gridspec as gridspec
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import pylab
+from scipy.special import softmax
+import h5py
+import os
+
+###################################
+###### REDSHIFT DISTRIBUTION ######
+###################################
+
+nzs = h5py.File('roman_nz_realizations/sc1b_d4/SVSN/nz_samples__LHC0_pointZ_1e6_Roman_sc1b_d4.h5','r') # nz simulations: z and nzs
+mean_nz_unorm = np.genfromtxt('results/zdistris/05_mean_roman_nz.txt') # unormalized
+
+def ni_gal():
+    """
+    input: unormalized redshift distribution
+    outputs: (used in CosmoCov inifile): source_n_gal = lens_n_gal = n_gal = fi/∑_{i=1}^9 (fi) where fi = ∫n_i(z) dz
+    """
+    int=[]
+    for i in range(mean_nz_unorm.shape[1]):
+        fi = [np.trapz(y=mean_nz_unorm[:,i], x=nzs["zbinsc"][:])]
+        int+=fi
+
+    tot = np.sum(int)
+    fractions = int/tot
+    return int,fractions # integral , ni_gal
+
+mean_nz_norm = mean_nz_unorm.copy()
+int , _ = ni_gal()
+for i in range(mean_nz_norm.shape[1]):
+    mean_nz_norm[:,i] = mean_nz_norm[:,i]/int[i]
+np.savetxt('results/zdistris/05_mean_roman_nz_normalized_sc1bd4.txt',mean_nz_norm)
+
+def make_z_nz_combined():
+    zbinsc = nzs["zbinsc"][:] # z center (cocoa)
+    zbinsmin = nzs["zbins"][:46] # z bin min (cosmocov) 
+
+    zbinsc_reshaped = zbinsc.reshape(-1, 1)  # shape (46, 1)
+    zbinsmin_reshaped = zbinsmin.reshape(-1, 1)  # shape (46, 1)
+
+    combined_zbinsc = np.hstack((zbinsc_reshaped, mean_nz_norm))
+    combined_zbinsmin = np.hstack((zbinsmin_reshaped, mean_nz_norm))
+
+    np.savetxt("results/zdistris/mean_roman_nzc_normalized_sc1bd4.nz", combined_zbinsc) # z mean + nzs (cocoa)
+    np.savetxt("results/zdistris/mean_roman_nzmin_normalized_sc1bd4.nz", combined_zbinsmin) # z min + nzs (cosmocov)
+    return None
+make_z_nz_combined()
--- codes/plot_modes.py
+++ codes/plot_modes.py
@@ -17,12 +17,11 @@ print(U['U'][0].shape)
 
 ### DES CASE ###
 N = (3-0)/0.01
-# z = np.linspace(0,3,int(N)-1)
-z = np.linspace(0,3,int(N))
+z = np.linspace(0,3,int(N)-1)
+# z = np.linspace(0,3,int(N))
 
 print(len(z))
 
-fig, axs = plt.subplots(nrows=2,ncols=4,sharey=False,sharex=False,figsize=(12,7))
 
 
 # U_fisher_pile3_3x2 = np.load('/gpfs/projects/MirandaGroup/Diogo/ROMAN-NZ-PROJECT/PCA/photoz_uz/NZ_COMPRESSION_OUTS/U_source_with_fisher_DES.npz')
@@ -30,28 +29,39 @@ fig, axs = plt.subplots(nrows=2,ncols=4,sharey=False,sharex=False,figsize=(12,7)
 
 # Us = [U_fisher_pile3_3x2, U_fisher_pile3_xip]
 
-Us = [np.load('./U_source.npz')]
 
-for k in np.arange(1):
-    U = Us[k]
-    for j in np.arange(4):
-        for i in np.arange(5):
-            if k == 0:
-                axs[k,j].plot(z,U['U'][i][j],label=rf'$\mathrm{{PC}}_{i}$' if j==0 else None)
-            elif k == 1:    
-                axs[k,j].plot(z,U['U'][i][j],label=rf'$\mathrm{{PC}}_{i}$' if j==0 else None)
-            axs[k,j].set_xlabel('z',fontsize=15)
-            if k == 0:
-                axs[k,j].set_title(f'Tomo. Bin: {j}' if j==0 else f'{j}')
-        axs[k,j].set_xlim(0,3)
-        axs[k,j].set_ylim(-0.2,0.2)
+def plot_modes():
+    fig, axs = plt.subplots(nrows=2,ncols=4,sharey=False,sharex=False,figsize=(12,7))
+    Us = [np.load('./U_source.npz')]
+    for k in np.arange(1):
+        U = Us[k]
+        for j in np.arange(4):
+            for i in np.arange(5):
+                if k == 0:
+                    axs[k,j].plot(z,U['U'][i][j],label=rf'$\mathrm{{PC}}_{i}$' if j==0 else None)
+                elif k == 1:    
+                    axs[k,j].plot(z,U['U'][i][j],label=rf'$\mathrm{{PC}}_{i}$' if j==0 else None)
+                axs[k,j].set_xlabel('z',fontsize=15)
+                if k == 0:
+                    axs[k,j].set_title(f'Tomo. Bin: {j}' if j==0 else f'{j}')
+            axs[k,j].set_xlim(0,3)
+            axs[k,j].set_ylim(-0.2,0.2)
 
-    if k == 0:
-        axs[k,0].legend(loc='upper right')
-        axs[k,0].set_ylabel('CosmoSIS\nDV pile3 w/ 3x2 pt',fontsize=15)
-    elif k == 1:    
-        axs[k,0].legend(loc='upper right')
-        axs[k,0].set_ylabel('CosmoSIS\nDV pile3 w/ '+r'$\xi_+$',fontsize=15)
+        if k == 0:
+            axs[k,0].legend(loc='upper right')
+            axs[k,0].set_ylabel('CosmoSIS\nDV pile3 w/ 3x2 pt',fontsize=15)
+        elif k == 1:    
+            axs[k,0].legend(loc='upper right')
+            axs[k,0].set_ylabel('CosmoSIS\nDV pile3 w/ '+r'$\xi_+$',fontsize=15)
 
-plt.tight_layout()
-plt.savefig('./test.pdf')
+    plt.tight_layout()
+    plt.savefig('./test.pdf')
+
+
+plot_modes()
+
+plt.figure()
+x = np.genfromtxt('chisq_kept.txt')
+plt.plot(list(range(len(x))),x,'o')
+plt.yscale('log')
+plt.savefig('test_chisq_kept.pdf')
