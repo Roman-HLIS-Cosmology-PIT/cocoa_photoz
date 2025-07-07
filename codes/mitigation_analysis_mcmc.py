3105258bc44c1521f36770c6e8173ff992248e8e
diogohf <diogohenriquefrsz@gmail.com>
2025-07-07 10:01:10 -0700

    adding dict with info - mcmc control

--- codes/mitigation_analysis_mcmc.py
+++ codes/mitigation_analysis_mcmc.py
@@ -4,49 +4,116 @@ from getdist import plots,loadMCSamples,MCSamples
 
 burnin=0.5
 settings = {"ignore_rows": burnin}
-# path = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/roman_sc1bd4_g/" # 8 decimal in nz (stoped)
-# path = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/roman_sc1bd4_g_18decimal/" # 18 decimal in nz (stoped)
-# path = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/roman_sc1bd4_test_dv/" # dv generated with modelvector.py
-# path = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/same_params_than_cosmocov/" # dv generated with modelvector.py
-path = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/diff_params_than_cosmocov/" # dv generated with modelvector.py
-# path1 = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/roman_sc1bd4_g/ROMAN_REAL_MCMC0_example1.modelvector" # example1.modelvector: 3x2 (original)
-# path2 = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/roman_sc1bd4_g/ROMAN_REAL_MCMC0_print_datavector_example1.modelvector" # cosmic shear only from print data vector
-# path3 = "/gpfs/scratch/pit-roman-hlis/Diogo/cocoapy310/Cocoa/cocoa_photoz/results/chains/roman_sc1bd4_g/ROMAN_REAL_MCMC0_print_datavector_example1_3x2pt.modelvector" # 3x2 from print data vector
-px = 'PCA_MODEL'
-# px = 'PCA_MODEL_modelvector.py'
-# px = 'PCA_MODEL_printdv_3x2'
+cp = '../results/chains' # common path
+mcmc_control = {'c_1':{'path':f'{cp}/roman_sc1bd4_g/',
+                       'prefix':'PCA_MODEL',
+                       'info':'8 decimal in nz. Status: (canceled)'},
+                'c_2':{'path':f'{cp}/roman_sc1bd4_g_18decimal/',
+                       'prefix':'PCA_MODEL',
+                       'info':'18 decimal in nz. Status: (canceled)'},
+                'c_3':{'path':f'{cp}/roman_sc1bd4_test_dv/',
+                       'prefix':'PCA_MODEL_modelvector.py',
+                       'info':'dv generated with modelvector.py. Status: (canceled)'},
+                'c_4':{'path':f'{cp}/same_params_than_cosmocov/',
+                       'prefix':'PCA_MODEL',
+                       'info':'dv generated with modelvector.py. Status: (running)'},
+                'c_5':{'path':f'{cp}/diff_params_than_cosmocov/',
+                       'prefix':'PCA_MODEL',
+                       'info':'dv generated with modelvector.py. Status: (running)'},
+                'c_6':{'path':f'{cp}/roman_real/',
+                       'prefix':'ROMAN_REAL',
+                       'info':'example1.modelvector: 3x2 (original)'},
+                'c_7':{'path':f'{cp}/roman_real/',
+                       'prefix':'PCA_MODEL',
+                       'info':'Same as c_5, but with Gaussian prior on alphas.'}}
 
-# no_pc = loadMCSamples(path+f"{px}_MCMC0", settings=settings)
-# one_pc = loadMCSamples(path+f"{px}_MCMC1", settings=settings)
-# two_pc = loadMCSamples(path+f"{px}_MCMC2", settings=settings)
-# three_pc = loadMCSamples(path+f"{px}_MCMC3", settings=settings)
+control = 'c_5'
+path = mcmc_control[control]['path']
+px = mcmc_control[control]['prefix']
+info = mcmc_control[control]['info']
+print(info)
 
+def roman_pca_mcmc():
+    no_pc = loadMCSamples(path+f"{px}_MCMC0", settings=settings)
+    one_pc = loadMCSamples(path+f"{px}_MCMC1", settings=settings)
+    two_pc = loadMCSamples(path+f"{px}_MCMC2", settings=settings)
+    three_pc = loadMCSamples(path+f"{px}_MCMC3", settings=settings)
 
-# g = plots.get_subplot_plotter()
-# g.plots_1d([no_pc,one_pc,two_pc,three_pc],["omegam","sigma8","w","wa"],
-#            markers={"omegam":0.3,"sigma8":0.8120,"w":-1,"wa":0}, # planck best-fit 1807.06209
-#            nx=4,legend_ncol=5,legend_labels=['No PC', '1 PC', '2 PC', '3 PC'],
-#            colors=['#1b5f6f',"#E69F00","#D55E00","#56B4E9"],
-#            ls=['-','--','-.',':'],lws=[3,3,3,3])
-# g.export("1d_marg.pdf")
+    g = plots.get_subplot_plotter()
+    g.plots_1d([no_pc,one_pc,two_pc,three_pc],["omegam","sigma8","w","wa"],
+            markers={"omegam":0.3,"sigma8":0.8120,"w":-1,"wa":0}, # planck best-fit 1807.06209
+            nx=4,legend_ncol=5,legend_labels=['No PC', '1 PC', '2 PC', '3 PC'],
+            colors=['#1b5f6f',"#E69F00","#D55E00","#56B4E9"],
+            ls=['-','--','-.',':'],lws=[3,3,3,3])
+    g.export("1d_marg.pdf")
+
+    g = plots.get_subplot_plotter()
+    g.plots_1d([one_pc],["roman_alpha_1"],
+            markers={"roman_alpha_1":0.0},
+            colors=["#E69F00"],
+            ls=['-'],lws=[3])
+    g.export("1d_marg_alpha_1pc.pdf")
+
+    g = plots.get_subplot_plotter()
+    g.plots_1d([two_pc],["roman_alpha_1","roman_alpha_2"],
+            markers={"roman_alpha_1":0.0,"roman_alpha_2":0.0},
+            colors=["#D55E00"],nx=2,
+            ls=['-'],lws=[3])
+    g.export("1d_marg_alpha_2pc.pdf")
+
+    g = plots.get_subplot_plotter()
+    g.plots_1d([three_pc],["roman_alpha_1","roman_alpha_2","roman_alpha_3"],
+            markers={"roman_alpha_1":0.0,"roman_alpha_2":0.0,"roman_alpha_3":0.0},
+            colors=["#56B4E9"],nx=3,
+            ls=['-'],lws=[3])
+    g.export("1d_marg_alpha_3pc.pdf")
+    return None
+roman_pca_mcmc()
+
+def roman_real_mcmc():
+    mcmc0 = loadMCSamples(path+f"{px}_MCMC0", settings=settings)
+    mcmc1 = loadMCSamples(path+f"{px}_MCMC1", settings=settings)
+    mcmc2 = loadMCSamples(path+f"{px}_MCMC2", settings=settings)
+
+    g = plots.get_subplot_plotter()
+    g.plots_1d([mcmc0,mcmc1,mcmc2],["omegam","sigma8","w","wa"],
+            markers={"omegam":0.3,"sigma8":0.8120,"w":-1,"wa":0}, # planck best-fit 1807.06209
+            nx=4,legend_ncol=5,
+            legend_labels=['ROMAN_REAL_MCMC0', 'ROMAN_REAL_MCMC1', 'ROMAN_REAL_MCMC2'],
+            colors=['#1b5f6f',"#E69F00","#D55E00"],
+            ls=['-','--','-.'],lws=[3,3,3])
+    g.export("1d_marg.pdf")
+
+    g = plots.get_subplot_plotter()
+    g.plots_1d([mcmc0,mcmc1,mcmc2],["roman_DZ_S1","roman_DZ_S2","roman_DZ_S3"],
+            markers={"roman_DZ_S1":0.0,"roman_DZ_S2":0.0,"roman_DZ_S3":0.0},
+            colors=['#1b5f6f',"#E69F00","#D55E00"],nx=3,
+            ls=['-','--','-.'],lws=[3,3,3])
+    g.export("1d_marg_alpha_3pc.pdf")
+    return None
 
 def plot_RMinus1():
     plt.figure()
     Rminus1_stop = 0.015
     max_val = 0
-    progress_0 = 'PCA_MODEL_MCMC0.progress'
-    progress_1 = 'PCA_MODEL_MCMC1.progress'
-    progress_2 = 'PCA_MODEL_MCMC2.progress'
-    progress_3 = 'PCA_MODEL_MCMC3.progress'
+    progress_0 = f'{px}_MCMC0.progress'
+    progress_1 = f'{px}_MCMC1.progress'
+    progress_2 = f'{px}_MCMC2.progress'
+    progress_3 = f'{px}_MCMC3.progress'
+
     progress = [progress_0,progress_1,progress_2,progress_3]
     colors = ['#1b5f6f',"#E69F00","#D55E00","#56B4E9"]
     legend_labels=['No PC', '1 PC', '2 PC', '3 PC']
+
+    # progress = [progress_0,progress_1,progress_2]
+    # colors = ['#1b5f6f',"#E69F00","#D55E00"]
+    # legend_labels=['ROMAN_REAL_MCMC0', 'ROMAN_REAL_MCMC1', 'ROMAN_REAL_MCMC2']
 
     for p,c,l in zip(progress,colors,legend_labels):
         Rminus1 = np.genfromtxt(path+p,usecols=(3))
         Rminus1 = Rminus1[~np.isnan(Rminus1)]
         vals = list(range(len(Rminus1)))
-        if np.max(vals) > max_val:
+        if  np.max(vals) > max_val:
             max_val = np.max(vals)
         plt.plot(vals,Rminus1,label=l,marker='o',markersize=3,color=c)
 
