35004c6ac1f7180228697131f34c6287477e28bd
diogohf <diogohenriquefrsz@gmail.com>
2025-07-08 11:08:11 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# cocoa_photoz
+# cocoa_photoz  
+MCMC PCA SCHEDULE PROGRAM  
+
+# PCA Method  
+A: PCs from Cn  
+B: PCs from nz_compression  
+
+# Dv used  
+same: dv generated with same params as used in CosmoCov  
+diff: dv generated with diff params as used in CosmoCov  
+
+# Prior on alpha:  
+Gauss: Gaussian (0,1)  
+Flat: Flat (-3,3)  
+
+#### MCMC  
+### Prior: Gauss  
+## Method A: PCs from Cn  
+# dv: same  
+0 / running  
+1 / running  
+2 / running  
+3 / running  
+# dv: diff  
+4 / running  
+5 / running  
+6 / running  
+7 / running  
+## Method B: PCs from nz_compression  
+# dv: same  
+8 = 0  
+9 / running  
+10 / running  
+11 / running  
+# dv: diff  
+12 = 4  
+13 / running  
+14 / running  
+15 / running  
+### Prior: Flat  
---
+++ sh/resume_mcmc.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -J PCA_same_gauss_A
+#SBATCH -o cocoa_photoz/results/outs_jacobian/%x_%A_%a.out
+#SBATCH -e cocoa_photoz/results/outs_jacobian/%x_%A_%a.err
+#SBATCH -p compute
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=7
+#SBATCH -t 10-00:00:00
+
+path=/gpfs/scratch/pit-roman-hlis/Diogo/cocoa8jul25/Cocoa/cocoa_photoz/yamls/roman_pca
+yaml=$path/MCMC${SLURM_ARRAY_TASK_ID}.yaml
+
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo Slurm job NAME is $SLURM_JOB_NAME
+echo Slurm job ID is $SLURM_JOBID
+echo Slurm submit DIR is $SLURM_SUBMIT_DIR
+
+cd $SLURM_SUBMIT_DIR
+module purge > /dev/null 2>&1
+module load slurm
+
+source /cm/shared/apps/miniforge/etc/profile.d/conda.sh
+conda activate cocoa
+source start_cocoa.sh
+
+export OMP_PROC_BIND=close
+if [ -n "$SLURM_CPUS_PER_TASK" ]; then
+  export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+else
+  export OMP_NUM_THREADS=1
+fi
+
+$CONDA_PREFIX/bin/mpirun -n ${SLURM_NTASKS} --oversubscribe --mca pml ^ucx --mca btl vader,tcp,self --bind-to core:overload-allowed --rank-by slot --map-by numa:pe=${OMP_NUM_THREADS} cobaya-run $yaml -r
